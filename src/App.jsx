c0eeb18a83a03ad56372bde93a8caa4e1a8a4931
Anandhu-Manoj <manojanandhu74@gmail.com>
2025-02-07 13:27:33 +0530

    mediaplayer

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Routes,Route } from "react-router-dom";
+import Content from "./Pages/Content";
+import Home from "./Pages/Home";
+import History from "./Pages/History";
+import Header from "./Pages/Header";
+import Footer from "./Pages/Footer";
+
+
+
+
+function App() {
+  return (
+    <>
+        <Header/>
+      <Routes>
+        <Route element={< Content/>} path='/'> </Route>
+        <Route element={< Home/>} path='/home'> </Route>
+        <Route element={< History/>} path='/history'> </Route>
+
+
+      </Routes>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
