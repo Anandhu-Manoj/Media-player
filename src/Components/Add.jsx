f9d3a7f3800c82b962b1c631f4827026806737e1
Anandhu-Manoj <manojanandhu74@gmail.com>
2025-02-12 20:32:05 +0530

    Updated feature/fixed bug/modified file

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -1,61 +1,123 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 
 const Add = () => {
-    const [show, setShow] = useState(false);
+  const [video, setVideo] = useState({
+    caption: "",
+    Image: "",
+    videoUrl: "",
+  });
+
+  const [error, setError] = useState(false);
+  const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  return (
-   <> 
-    <div className=''>
-      <div className="d-flex align-center gap-5 ">
-        <h2 className="">Upload videos </h2>{" "}
-        <button onClick={handleShow} className="btn btn-warning rounded-circle">
-          <i className="fa-solid fa-plus   fw-bolder fs-5 "></i>
-        </button>
-      </div>
-    </div>
-     <Modal centered  show={show} onHide={handleClose} animation={false} >
-     <Modal.Header closeButton>
-       <Modal.Title>Upload VIdeos</Modal.Title>
-     </Modal.Header>
-     <Modal.Body style={{border:"1px solid white ",borderRadius:"15px"}}> <div><FloatingLabel
-        controlId="floatingInput"
-        label="Video Caption"
-        className="mb-3 "
-        
-      >
-        <Form.Control type="text" placeholder="Video Caption" />
-      </FloatingLabel>
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Video image URL"
-        className="mb-3"
-      >
-        <Form.Control type="text" placeholder="Video image URL" />
-      </FloatingLabel>
+  const seperateYoutubeUrl = (value) => {
+    if (value.includes(".be/")) {
+      const videoId = value.split("be/")[1];
+      setVideo({...video,videoUrl:videoId})
+
+      console.log(videoId);
+      setError(false);
+    } else {
+      console.error("invalid utube Link");
+      setError(true);
+    }
+  };
+  const handleSave=()=>{
+    if(video.caption&&video.image&&video.videoUrl){
+      console.log("success");
       
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Youtube Link"
-        className="mb-3"
-      >
-        <Form.Control type="text" placeholder="Youtube Link" />
-      </FloatingLabel></div> </Modal.Body>
-     <Modal.Footer>
-       <Button variant="secondary" onClick={handleClose}>
-         Close
-       </Button>
-       <Button variant="primary" onClick={handleClose}>
-         Save Changes
-       </Button>
-     </Modal.Footer>
-   </Modal>
-   </>
+    }else{
+      alert("please fill the form")
+    }
+
+  }
+
+  
+  return (
+    <>
+      <div className="">
+        <div className="d-flex align-center gap-5 ">
+          <h2 className="">Upload videos </h2>{" "}
+          <button
+            onClick={handleShow}
+            className="btn btn-warning rounded-circle"
+          >
+            <i className="fa-solid fa-plus   fw-bolder fs-5 "></i>
+          </button>
+        </div>
+      </div>
+      <Modal centered show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Upload VIdeos</Modal.Title>
+        </Modal.Header>
+        <Modal.Body
+          style={{ border: "1px solid white ", borderRadius: "15px" }}
+        >
+          {" "}
+          <div>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Video Caption"
+              className="mb-3 "
+            >
+              <Form.Control
+                onChange={(e) => {
+                  setVideo({ ...video, caption: e.target.value });
+                }}
+                type="text"
+                placeholder="Video Caption"
+              />
+            </FloatingLabel>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Video image URL"
+              className="mb-3"
+            >
+              <Form.Control
+                onChange={(e) => {
+                  setVideo({ ...video, image: e.target.value });
+                }}
+                type="text"
+                placeholder="Video image URL"
+              />
+            </FloatingLabel>
+
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Youtube Link"
+              className="mb-3"
+            >
+              <Form.Control
+                onChange={(e) => seperateYoutubeUrl(e.target.value)}
+                type="text"
+                placeholder="Youtube Link"
+              />
+            </FloatingLabel>
+            {error ? (
+              <div>
+                <p className="text-danger">invalid url link</p>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>{" "}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSave}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
--- src/Components/Catogories.jsx
+++ src/Components/Catogories.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 
 const Catogories = () => {
-  return (
-    <div>Catogories</div>
-  )
-}
+  const [show, setShow] = useState(false);
 
-export default Catogories
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+      <div>
+        <div className="d-flex ">
+          <h2>all Caatogories</h2>
+          <button
+            onClick={handleShow}
+            className="btn btn-warning rounded-circle"
+          >
+            <i className="fa-solid fa-plus   fw-bolder fs-5 "></i>
+          </button>
+        </div>
+      </div>
+      <Modal centered show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Upload Catogory Details!!!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body
+          style={{ border: "1px solid white ", borderRadius: "15px" }}
+        >
+          {" "}
+          <div>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Catogory Name"
+              className="mb-3 "
+            >
+              
+
+              <Form.Control type="text" placeholder="Youtube Link" />
+            </FloatingLabel>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default Catogories;
